ab6bf7a70b4806e45713b941220cd85e21934876
tribhuwan-kumar <tribhuwankumar725275@gmail.com>
2025-05-28 15:19:39 +0530

    fix: add example

--- examples/terminator-rust-examples/win_automation/src/main.rs
+++ examples/terminator-rust-examples/win_automation/src/main.rs
@@ -1,4 +1,4 @@
-use terminator::{AutomationError, Desktop, Selector};
+use terminator::{AutomationError, Desktop, Selector, platforms};
 use tracing::Level;
 
 // like a playground, just uncomment
@@ -97,7 +97,7 @@ async fn main() -> Result<(), AutomationError> {
     // Or, just open notepad blank first, then finding the edit area and setting its value.
     // Let's try opening notepad blank first, then finding the edit area and setting its value.
     // let notepad_app = engine.open_application("notepad")?;
-    let desktop = Desktop::new(false, true).await.unwrap();
+    // let desktop = Desktop::new(false, true).await.unwrap();
     // desktop.activate_browser_window_by_title("Excel")?;
     // let window = desktop.get_current_browser_window().await.unwrap();
     // println!("window: {:?}", window.attributes());
@@ -118,28 +118,28 @@ async fn main() -> Result<(), AutomationError> {
 
     // in firefox, get all edit elements and type hello in each of them
     // let app = desktop.application("firefox").unwrap();
-    let window = desktop.get_current_browser_window().await.unwrap();
-    let elements = window
-        .locator(Selector::Role {
-            role: "edit".to_string(),
-            name: None,
-        })
-        .unwrap()
-        .all(None, None)
-        .await
-        .unwrap();
-    for element in elements.iter() {
-        // element.type_text("hello", true)?;
-
-        if element.is_keyboard_focusable().unwrap_or(false) {
-            println!("element: {:?}", element.attributes());
-            println!("element: {:?}", element.id());
-            // element.type_text("hello a very long text that describe how the universe works", true)?;
-        }
-
-        // println!("element: {:?}", element.attributes());
-        // println!("element: {:?}", element.id());
-    }
+    // let window = desktop.get_current_browser_window().await.unwrap();
+    // let elements = window
+    //     .locator(Selector::Role {
+    //         role: "edit".to_string(),
+    //         name: None,
+    //     })
+    //     .unwrap()
+    //     .all(None, None)
+    //     .await
+    //     .unwrap();
+    // for element in elements.iter() {
+    //     // element.type_text("hello", true)?;
+    //
+    //     if element.is_keyboard_focusable().unwrap_or(false) {
+    //         println!("element: {:?}", element.attributes());
+    //         println!("element: {:?}", element.id());
+    //         // element.type_text("hello a very long text that describe how the universe works", true)?;
+    //     }
+    //
+    //     // println!("element: {:?}", element.attributes());
+    //     // println!("element: {:?}", element.id());
+    // }
 
     // Debug logging
     // println!("Found Cursor application");
@@ -449,6 +449,48 @@ async fn main() -> Result<(), AutomationError> {
     //     // let _ = element.locator(sclb)?;
     //     // // scroll the scrollbar
     //     // sclb_ele.scroll("down", 10.0)?;
+
+    // example for `name along with role`
+    let engine = platforms::create_engine(true, true)?;
+
+    let opened_app = engine.open_application("uwp:Microsoft.MicrosoftEdge.Stable")?;
+
+    // target more specific!!
+    let tool_bar = Selector::Role { 
+        role: "ToolBar".to_string(), 
+        name: Some("App bar".to_string()),         
+    };
+
+    let tool_bar = engine.find_element(&tool_bar, Some(&opened_app), None)?;
+
+    // this will get any random button, regardless of its name
+    let rand_button = Selector::Role { 
+        role: "Button".to_string(), 
+        name: None,         
+    };
+
+    let rand_button_ele = engine.find_element(&rand_button, Some(&tool_bar), None)?;
+    rand_button_ele.click()?;
+
+    std::thread::sleep(std::time::Duration::from_millis(3000));
+
+    // this will get the button with the specific name of `Settings and more`
+    let three_dots = Selector::Role { 
+        role: "Button".to_string(), 
+        name: Some("Settings and more".to_string()),         
+    };
+    let rand_button_ele = engine.find_element(&three_dots, Some(&tool_bar), None)?;
+    rand_button_ele.click()?;
+
+    std::thread::sleep(std::time::Duration::from_millis(3000));
+
+    let split_screen = Selector::Role { 
+        role: "Menuitem".to_string(), 
+        name: Some("Split screen".to_string()),         
+    };
+
+    let split_screen_ele = engine.find_element(&split_screen, Some(&opened_app), None)?;
+    split_screen_ele.click()?;
 
     Ok(())
 }
