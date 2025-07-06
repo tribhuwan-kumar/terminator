5b2191e641b2ac574fcbb5871f7c9a628aeced26
divanshu-go <divanshugrover2009@gmail.com>
2025-07-06 20:45:04 +0530

    fix doc tests

--- terminator-workflow-recorder/src/recorder.rs
+++ terminator-workflow-recorder/src/recorder.rs
@@ -136,6 +136,8 @@ pub struct WorkflowRecorderConfig {
     /// # Examples
     ///
     /// ```rust
+    /// use terminator_workflow_recorder::WorkflowRecorderConfig;
+    ///
     /// let mut config = WorkflowRecorderConfig::default();
     /// config.enable_multithreading = true;  // Use multithreaded COM (MTA)
     /// config.enable_multithreading = false; // Use apartment threaded COM (STA) - default
