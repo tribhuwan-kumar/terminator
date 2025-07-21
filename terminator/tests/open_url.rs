7dd1a60923477216001500d9f3f1a24ac60bccea
tribhuwan-kumar <tribhuwankumar725275@gmail.com>
2025-07-21 15:34:27 +0530

    fix: use different and wider test cases

--- terminator/tests/open_url.rs
+++ terminator/tests/open_url.rs
@@ -11,23 +11,25 @@ async fn test_open_url() -> Result<(), AutomationError> {
 
     let engine = platforms::create_engine(false, false)?;
 
-    let url = "https://github.com/mediar-ai/terminator/issues/180";
-
-    let browsers = vec![
-        Browser::Default,
-        Browser::Chrome,
-        Browser::Firefox,
-        Browser::Edge,
-        Browser::Brave,
-        Browser::Opera,
-        Browser::Vivaldi,
+    let cases = vec![
+        ("https://www.mediar.ai/", Browser::Default),
+        ("https://www.reddit.com/", Browser::Chrome),
+        ("https://stackoverflow.com/", Browser::Firefox),
+        ("https://github.com/mediar-ai/terminator/issues/180", Browser::Edge),
+        ("https://practicetestautomation.com/practice-test-login/", Browser::Brave),
+        ("https://docs.screenpi.pe/terminator/introduction", Browser::Opera),
+        ("https://thisisatest.com/", Browser::Default), // invalid url
     ];
 
-    for app in browsers {
-        let result = engine.open_url(url, Some(app))?;
+    for (url, browser) in cases {
+        let result = engine.open_url(url, Some(browser.clone()));
         tokio::time::sleep(Duration::from_secs(5)).await;
 
-        assert!(result.name().is_some() , "Expected failure when opening URL with an invalid custom browser");
+        if url == "https://thisisatest.com/" {
+            assert!(result.is_err(), "expected failure for invalid url");
+        } else {
+            assert!(result?.name().is_some(), "expected name for ui element");
+        }
     }
     Ok(())
 }
